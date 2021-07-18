826b65a6f8f51c0898d68b251a2427c4a6935025
Evgenii Semenov <eo.semenoff@gmail.com>
2021-07-18 22:37:15 +0300

    PostmanEchoPostTest with fail added

---
+++ src/test/java/PostmanEchoPostTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoPostTest {
+    @Test
+    void shouldSendUserData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some test data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some test data-1"))
+        ;
+    }
+}
